5f6f891d2ef405790015676c7f31f9cffc474410
Ghanim <pspsony200888@gmail.com>
2017-11-20 14:42:20 +0300

    made sure w and h are > 0

--- src/MyRectangle.java
+++ src/MyRectangle.java
@@ -4,20 +4,22 @@ public class MyRectangle {
 	public double w, h, a, p;
 
 	public void printRectangle() {
+		if (w > 0 && h > 0) {
+			for (int c = 1; c <= w; c++) {
+				for (int c2 = 1; c2 <= h; c2++) {
+					System.out.print("*");
+				}
+				System.out.println();
 
-		for (int c = 1; c <= w; c++) {
-			for (int c2 = 1; c2 <= h; c2++) {
-				System.out.print("*");
 			}
-			System.out.println();
-
 		}
-
 	}
+
 	public void result() {
 		a = w * h;
-		p = (2*w) + (2*h);
-		System.out.println("The area of a rectangle with width "+w+" and height "+h+" is " +a+" The perimeter of a rectangle is "+p);
+		p = (2 * w) + (2 * h);
+		System.out.println("The area of a rectangle with width " + w + " and height " + h + " is " + a
+				+ " The perimeter of a rectangle is " + p);
 	}
 
 }
